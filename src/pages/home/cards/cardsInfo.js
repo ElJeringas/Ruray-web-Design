6f8caac89216c15908440eadc051be07b6c559e2
ElJeringas <santiagojss32@gmail.com>
2021-11-04 11:52:04 -0500

    prototype and design of home page

---
+++ src/pages/home/cards/cardsInfo.js
@@ -0,0 +1,8 @@
+
+
+const cardInfo = [
+    {key:0,image:lands,title:"Fincas",text:"crear Fincas",go:(e => Finca()),get:(e => getLand())},
+    {key:0,image:animal,title:"Animales",text:"crear animales",go:(e => Animal()),get:(e => getAnimal())},
+    {key:0,image:breed,title:"Razas",text:"crear razas",go:(e => Breeds()),get:(e => getBreed()) },
+    
+]; 
